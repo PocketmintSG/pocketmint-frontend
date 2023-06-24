06de055c791ca6d1a72b2c8f6e0061c51101c3b3
Mai Ting Kai <wheatgrass.mc@gmail.com>
2023-06-25 00:18:04 +0800

    Remove unused files

--- src/main.jsx
+++ src/main.jsx
@@ -2,46 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Root, { loader as rootLoader, action as rootAction } from './routes/root'
+import Root from './routes/root'
 import ErrorPage from './general/error-page'
-import Contact, { loader as contactLoader, action as contactAction } from './routes/contact'
-import EditContact, {
-  action as editAction,
-} from './routes/edit'
-import { action as destroyAction } from "./routes/destroy";
-import Index from './routes/Index'
 
 const router = createBrowserRouter([{
   path: "/",
   element: <Root />,
   errorElement: <ErrorPage />,
-  loader: rootLoader,
-  action: rootAction,
-  children: [
-    {
-      errorElement: <ErrorPage />,
-      children: [
-        { index: true, element: <Index /> },
-        {
-          path: "contacts/:contactId",
-          element: <Contact />,
-          loader: contactLoader,
-          action: contactAction
-        },
-        {
-          path: "contacts/:contactId/edit",
-          element: <EditContact />,
-          loader: contactLoader,
-          action: editAction,
-        },
-        {
-          path: "contacts/:contactId/destroy",
-          action: destroyAction,
-          errorElement: <div>Oops! There was an error.</div>,
-        }
-      ]
-    }
-  ]
 },
 ]) 
 
--- src/routes/destroy.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { redirect } from "react-router-dom";
-import { deleteContact } from "../contacts";
-
-export async function action({ params }) {
-    await deleteContact(params.contactId);
-    return redirect("/");
-}
