fb67b3f028685d5bdcd01f0baf84b44cd8def8f7
ferusel <wheatgrass.mc@gmail.com>
2023-07-01 18:03:00 +0800

    Fix import

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { router } from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store.js"
+import { store } from "./redux/store"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
