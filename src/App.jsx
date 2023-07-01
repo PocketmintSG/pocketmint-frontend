5bb933549d976727b2c737edc4a6112186287bab
ferusel <wheatgrass.mc@gmail.com>
2023-07-01 18:02:53 +0800

    Remove unused component

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,10 @@ import Home from "./Home"
 import Login from "./auth/Login"
 import { ProtectedLayout } from "./ProtectedLayout"
 import { UnprotectedLayout } from "./UnprotectedLayout"
-import { AuthLayout } from "./AuthLayout"
-
-const getUserData = () =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      const user = window.localStorage.getItem("user");
-      console.log(user)
-      resolve(user);
-    }, 3000)
-  );
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={ <AuthLayout /> } loader={ () => defer({ userPromise: getUserData() }) }>
+    <Route>
       <Route element={ <ProtectedLayout /> }>
         <Route path="/" element={ <Home /> } />
       </Route>
