0bf3e014ea298ff171fa31d91c3785c5c5647654
ferusel <wheatgrass.mc@gmail.com>
2023-07-01 19:47:36 +0800

    Add firebase auth

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-MYIPxArSkkzyS0Gig-5JqX0iM4WC7mg",
+  authDomain: "pocketmint-frontend.firebaseapp.com",
+  projectId: "pocketmint-frontend",
+  storageBucket: "pocketmint-frontend.appspot.com",
+  messagingSenderId: "106285860508",
+  appId: "1:106285860508:web:05281520dc7d89150c4f59",
+  measurementId: "G-SDTG6MF8V9"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
