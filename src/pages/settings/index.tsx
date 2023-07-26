da84d7c83d12524350ba12dd8564e711bae72543
Mai Ting Kai <wheatgrass.mc@gmail.com>
2023-07-26 10:05:20 +0800

    Fix positioning of form fields

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -34,54 +34,58 @@ export const Settings = () => {
     triggerGenericNotification("Password updated successfully", "success")
   }
   return <Container className="bg-containerBackground h-[100vh] flex flex-col gap-10">
-    {isUpdatingPersonalInfo &&
+    {!isUpdatingPersonalInfo &&
       <Card className="p-10 -mt-3 z-10">
         <CardTitle className="text-lg mb-6">Personal Information</CardTitle>
-        <CardContent className="p-0 grid grid-cols-3">
-          <div className="flex flex-row items-center w-full gap-14 col-span-2">
-            <img src={HeinrichProfilePicture} alt="Your Image" className="object-cover w-[20%] h-full" />
-            <div className="flex flex-col gap-4">
-              <Label labelTitle="Username" labelContent="heinrich_lee" />
+        <CardContent className="p-0 h-[30vh] grid grid-rows-[80%_20%] gap-6">
+          <div className="row-span-1 grid grid-cols-4 gap-10">
+            <img src={HeinrichProfilePicture} alt="Your Image" className="object-cover h-full col-span-1" />
+            <div className="col-span-3 grid grid-cols-4 grid-rows-3 gap-4">
+              <div className="row-span-1 col-span-4">
+                <Label labelTitle="Username" labelContent="heinrich_lee" />
+              </div>
 
-              <div className="flex flex-row gap-[10rem]">
-                <Label labelTitle="First Name" labelContent="Heinrich" />
-                <Label labelTitle="Last Name" labelContent="Lee" />
+              <div className="row-span-1 col-span-4 grid grid-cols-4">
+                <Label className="col-span-2" labelTitle="First Name" labelContent="Heinrich" />
+                <Label className="col-span-2" labelTitle="Last Name" labelContent="Lee" />
               </div>
 
               <div>
-                <Label labelTitle="Email address" labelContent="heinrich_lee@gmail.com" />
+                <Label className="row-span-1 col-span-4" labelTitle="Email address" labelContent="heinrich_lee@gmail.com" />
               </div>
             </div>
           </div>
-          <div className="col-span-1 flex items-end justify-end gap-2">
+          <div className="row-span-1 flex items-center justify-end">
             <ButtonFilled onClick={() => setIsUpdatingPersonalInfo(!isUpdatingPersonalInfo)}>Update Information</ButtonFilled>
           </div>
         </CardContent>
       </Card>
     }
-    {!isUpdatingPersonalInfo &&
+    {isUpdatingPersonalInfo &&
       <Card className="p-10 -mt-3 z-10">
         <CardTitle className="text-lg mb-6">Personal Information</CardTitle>
-        <CardContent className="p-0 grid grid-cols-3">
-          <Formik className="w-full">
+        <CardContent className="p-0 h-[30vh] grid grid-rows-[80%_20%] gap-6">
+          <Formik>
             {({ isSubmitting }) => (
               <>
-                <Form className="flex flex-row items-center w-full gap-14 col-span-2">
-                  <img src={HeinrichProfilePicture} alt="Your Image" className="object-cover w-[20%] h-full" />
-                  <div className="flex flex-col gap-4">
-                    <FormInput className="w-[80%]" name="username" type="text" label="Username" labelProps="font-medium text-darkGrey-600 text-sm" />
-
-                    <div className="flex flex-row gap-[10rem]">
-                      <FormInput className="w-[60%]" name="firstName" type="text" label="First Name" labelProps="font-medium text-darkGrey-600 text-sm" />
-                      <FormInput className="w-[60%]" name="lastName" type="text" label="Last Name" labelProps="font-medium text-darkGrey-600 text-sm" />
+                <Form className="row-span-1 grid grid-cols-4 gap-10">
+                  <img src={HeinrichProfilePicture} alt="Your Image" className="object-cover h-full col-span-1" />
+                  <div className="col-span-3 grid grid-cols-4 grid-rows-3">
+                    <div className="row-span-1 col-span-4">
+                      <FormInput name="username" type="text" label="Username" labelProps="font-medium text-darkGrey-600 text-sm" />
                     </div>
 
-                    <div>
+                    <div className="row-span-1 col-span-4 grid grid-cols-4 gap-20">
+                      <FormInput className="col-span-2" name="firstName" type="text" label="First Name" labelProps="font-medium text-darkGrey-600 text-sm" />
+                      <FormInput className="col-span-2" name="lastName" type="text" label="Last Name" labelProps="font-medium text-darkGrey-600 text-sm" />
+                    </div>
+
+                    <div className="row-span-1 col-span-4">
                       <FormInput name="email" type="text" label="Email Address" labelProps="font-medium text-darkGrey-600 text-sm" />
                     </div>
                   </div>
                 </Form>
-                <div className="col-span-1 flex items-end justify-end gap-2">
+                <div className="row-span-1 flex items-center justify-end gap-4">
                   <ButtonGhost onClick={() => setIsUpdatingPersonalInfo(!isUpdatingPersonalInfo)}>Cancel</ButtonGhost>
                   <ButtonFilled onClick={() => setIsUpdatingPersonalInfo(!isUpdatingPersonalInfo)}>Confirm</ButtonFilled>
                 </div>
