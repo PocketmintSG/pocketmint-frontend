9512b4ea0efb3d6221c414bd061bc96fcba74ce6
Mai Ting Kai <wheatgrass.mc@gmail.com>
2023-07-26 16:33:39 +0800

    Add API Playground

--- src/pages/APIPlayground.tsx
+++ src/pages/APIPlayground.tsx
@@ -1,14 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import { ButtonFilled } from "@/components/general/buttons/ButtonFilled"
-import { loginUserAPI } from "@/api/auth";
+import { Field, Form, Formik } from "formik";
+import { UploadImageAPI } from "@/api/file_uploads";
+import * as Yup from "yup"
 
 export const APIPlayground = () => {
-  const handleClick = async () => {
-    console.log("Calling API");
-    const res = await loginUserAPI("test")
+  const [image, setImage] = useState<File>()
+
+  const handleFileChange = (event) => {
+    setImage(event.target.files[0])
+  }
+
+  const handleFileUpload = async () => {
+    const res = await UploadImageAPI(image!); // guaranteed to be defined due to Formik validation
     console.log(res)
   }
+
   return <div className="w-[100vw] h-[100vh] bg-containerBackground">
-    <ButtonFilled onClick={handleClick}>Make API Call</ButtonFilled>
+    <Formik initialValues={{
+      image: null
+    }} onSubmit={handleFileUpload} validationSchema={{
+      image: Yup.mixed().required("Image is required")
+    }}>
+      <Form>
+        <label htmlFor="image">Upload Image</label>
+        <Field type="file" name="image" onChange={handleFileChange}>
+          {({ field }) => (
+            <input type="file" {...field} onChange={handleFileChange} />
+          )}
+        </Field>
+        <ButtonFilled type="submit" onClick={handleFileUpload}>Get URL</ButtonFilled>
+      </Form>
+    </Formik>
   </div>
 }
