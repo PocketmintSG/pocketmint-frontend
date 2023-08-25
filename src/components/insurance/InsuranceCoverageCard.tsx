e0c421d8e3d839fc75139cd27bb2cd300d7faa8e
ferusel <wheatgrass.mc@gmail.com>
2023-08-25 17:31:21 +0200

    Add summary section cards

--- src/components/insurance/InsuranceCoverageCard.tsx
+++ src/components/insurance/InsuranceCoverageCard.tsx
@@ -3,27 +3,28 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
 export interface InsuranceCoverageCardProps {
-    totalAnnualPremiums: number
-    life: {
-        term: boolean,
-        wholeLife: boolean
-        endowment: boolean
-        annuity: boolean
+    "total_annual_premiums": number
+    "life": {
+        "term": boolean,
+        "whole_life": boolean
+        "universal_life": boolean
     }
-    accidentAndHealth: {
-        medical: boolean
-        longTermCare: boolean
-        criticalIllness: boolean
-        disability: boolean
-        personalAccident: boolean
+    "accident_and_health": {
+        "hospitalization": boolean
+        "critical_illness": boolean
+        "personal_accident": boolean
+        "private_integrated_shield_plans": boolean
     }
-    others: {
-        otherPolicies: boolean
+    "investments": {
+        "endowment": boolean
+        "investment_related": boolean
+        "retirement": boolean
     }
+    "others": boolean
     className: string
 }
 
-export const InsuranceCoverageCard = ({ totalAnnualPremiums, life, accidentAndHealth, others, className }: InsuranceCoverageCardProps) => {
+export const InsuranceCoverageCard = ({ total_annual_premiums: totalAnnualPremiums, life, accident_and_health: accidentAndHealth, investments, others, className }: InsuranceCoverageCardProps) => {
     return <Card className={className}>
         <CardHeader>
             <CardTitle className="text-lg">Insurance Coverage</CardTitle>
@@ -33,39 +34,49 @@ export const InsuranceCoverageCard = ({ totalAnnualPremiums, life, accidentAndHe
                 <span className="font-light">Total Annual Premiums</span>
                 <p className="font-semibold text-1.5xl">SGD ${totalAnnualPremiums}</p>
             </div>
-            <div className="flex flex-col gap-10">
-                <div className="flex flex-row gap-10">
+            <div className="flex flex-col gap-2 w-full">
+                <div className="flex flex-row w-full">
                     <img src="/src/assets/insurance/LifeLogo.svg" alt="Life Logo" className="w-[60px] h-[60px]" />
-                    <div>
+                    <div className="ml-10">
                         <span className="font-semibold text-md">Life</span>
-                        <ul className="grid grid-cols-2 grid-rows-2 gap-3">
+                        <ul className="grid grid-cols-2 grid-rows-2">
                             <InsuranceCoverageItemWrapper label="Term" checked={life.term} />
-                            <InsuranceCoverageItemWrapper label="Endowment" checked={life.endowment} />
-                            <InsuranceCoverageItemWrapper label="Whole Life" checked={life.wholeLife} />
-                            <InsuranceCoverageItemWrapper label="Annuity" checked={life.annuity} />
+                            <InsuranceCoverageItemWrapper label="Whole Life" checked={life.whole_life} />
+                            <InsuranceCoverageItemWrapper label="Universal Life" checked={life.universal_life} />
                         </ul>
                     </div>
                 </div>
-                <div className="flex flex-row gap-10">
+                <div className="flex flex-row w-full">
                     <img src="/src/assets/insurance/AccidentAndHealthLogo.svg" alt="Accident and Health Logo" className="w-[60px] h-[60px]" />
-                    <div>
+                    <div className="ml-10">
                         <span className="font-semibold text-md">Accident & Health</span>
-                        <ul className="grid grid-cols-2 grid-rows-2 gap-3">
-                            <InsuranceCoverageItemWrapper label="Medical" checked={accidentAndHealth.medical} />
-                            <InsuranceCoverageItemWrapper label="Critical Illness" checked={accidentAndHealth.criticalIllness} />
-                            <InsuranceCoverageItemWrapper label="Personal Accident" checked={accidentAndHealth.personalAccident} />
-                            <InsuranceCoverageItemWrapper label="Long Term Care" checked={accidentAndHealth.longTermCare} />
-                            <InsuranceCoverageItemWrapper label="Disability" checked={accidentAndHealth.disability} />
+                        <ul className="grid grid-cols-2 grid-rows-2">
+                            <InsuranceCoverageItemWrapper label="Hospitalization" checked={accidentAndHealth.hospitalization} />
+                            <InsuranceCoverageItemWrapper label="Critical Illness" checked={accidentAndHealth.critical_illness} />
+                            <InsuranceCoverageItemWrapper label="Personal Accident" checked={accidentAndHealth.personal_accident} />
+                            <InsuranceCoverageItemWrapper label="Integrated Shield Plans" checked={accidentAndHealth.private_integrated_shield_plans} />
                         </ul>
                     </div>
                 </div>
 
-                <div className="flex flex-row gap-10">
+                <div className="flex flex-row w-full">
+                    <img src="/src/assets/insurance/InsuranceLogo.svg" alt="Insurance Logo" className="w-[60px] h-[60px]" />
+                    <div className="ml-10">
+                        <span className="font-semibold text-md">Investments</span>
+                        <ul className="grid grid-cols-2 grid-rows-2">
+                            <InsuranceCoverageItemWrapper label="Endowment" checked={investments.endowment} />
+                            <InsuranceCoverageItemWrapper label="Investment-Related" checked={investments.investment_related} />
+                            <InsuranceCoverageItemWrapper label="Retirement" checked={investments.retirement} />
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="flex flex-row w-full">
                     <img src="/src/assets/insurance/OthersLogo.svg" alt="Others Logo" className="w-[60px] h-[60px]" />
-                    <div>
+                    <div className="ml-10">
                         <span className="font-semibold text-md">Others</span>
-                        <ul className="grid grid-cols-2 grid-rows-2 gap-3">
-                            <InsuranceCoverageItemWrapper label="Other Policies" checked={others.otherPolicies} />
+                        <ul className="grid grid-cols-2 grid-rows-2">
+                            <InsuranceCoverageItemWrapper label="Other Policies" checked={others} />
                         </ul>
                     </div>
                 </div>
--- src/components/insurance/MaxSumInsuredCard.tsx
+++ src/components/insurance/MaxSumInsuredCard.tsx
@@ -2,20 +2,25 @@ import { InsuranceCoverageItemWrapper } from "@/components/insurance/InsuranceCo
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
+interface MaxTotalPremium {
+    "_id": null,
+    "max_total_premiums": number
+}
 export interface InsuranceCoverageCardProps {
-    life: number
-    accidentAndHealth: number
-    others: number
+    "life": number
+    "accident_and_health": number
+    "investments": number
+    "others": number
     className: string
 }
 
-export const MaxSumInsuredCard = ({ life, accidentAndHealth, others, className }: InsuranceCoverageCardProps) => {
+export const MaxSumInsuredCard = ({ life, "accident_and_health": accidentAndHealth, investments, others, className }: InsuranceCoverageCardProps) => {
     return <Card className={className}>
         <CardHeader>
             <CardTitle className="text-lg">Maximum Sum Insured</CardTitle>
         </CardHeader>
-        <CardContent className="flex flex-col gap-5">
-            <div className="flex flex-col gap-10">
+        <CardContent>
+            <div className="flex flex-col gap-5">
                 <div className="flex flex-row gap-10">
                     <img src="/src/assets/insurance/LifeLogo.svg" alt="Life Logo" className="w-[60px] h-[60px]" />
                     <div className="flex flex-col gap-2">
@@ -26,11 +31,17 @@ export const MaxSumInsuredCard = ({ life, accidentAndHealth, others, className }
                 <div className="flex flex-row gap-10">
                     <img src="/src/assets/insurance/AccidentAndHealthLogo.svg" alt="Accident and Health Logo" className="w-[60px] h-[60px]" />
                     <div className="flex flex-col gap-2">
-                        <span className="font-semibold text-md">Life</span>
+                        <span className="font-semibold text-md">Accident and Health</span>
                         <p className="font-light">Medical: ${accidentAndHealth}</p>
                     </div>
                 </div>
-
+                <div className="flex flex-row gap-10">
+                    <img src="/src/assets/insurance/InsuranceLogo.svg" alt="Accident and Health Logo" className="w-[60px] h-[60px]" />
+                    <div className="flex flex-col gap-2">
+                        <span className="font-semibold text-md">Investments</span>
+                        <p className="font-light">Total Payments: ${investments}</p>
+                    </div>
+                </div>
                 <div className="flex flex-row gap-10">
                     <img src="/src/assets/insurance/OthersLogo.svg" alt="Others Logo" className="w-[60px] h-[60px]" />
                     <div>
@@ -39,7 +50,6 @@ export const MaxSumInsuredCard = ({ life, accidentAndHealth, others, className }
                     </div>
                 </div>
             </div>
-
         </CardContent>
     </Card >
 }
