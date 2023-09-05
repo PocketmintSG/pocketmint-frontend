cdc3ca4698d2a02c75a172c7f900ea92dbbdb233
ferusel <wheatgrass.mc@gmail.com>
2023-09-06 00:30:25 +0200

    Fix display username and profile picture on menu

--- src/components/general/menus/sidebar/NavSidebar.tsx
+++ src/components/general/menus/sidebar/NavSidebar.tsx
@@ -4,67 +4,69 @@ import { Link } from "react-router-dom";
 import HeinrichProfilePicture from "src/assets/placeholders/profile-picture-heinrich.svg";
 import PocketmintLogo from "src/assets/common/Logo_PocketMint.svg";
 import { NavigationItem } from "src/configs/Navigation";
+import { getUser } from "@/utils/Store";
 
 interface NavSidebarProps {
-  menuItems: NavigationItem[];
-  activeSection: NavigationItem;
-  setActiveSection: (newSection: NavigationItem) => void
+    menuItems: NavigationItem[];
+    activeSection: NavigationItem;
+    setActiveSection: (newSection: NavigationItem) => void
 }
 
 export const NavSidebar = ({
-  menuItems,
-  activeSection,
-  setActiveSection,
+    menuItems,
+    activeSection,
+    setActiveSection,
 }: NavSidebarProps) => {
-  const handleActiveMenuItem = (menuItemLabel: string) => {
-    const menuItem = menuItems.find((item) => item.menuLabel === menuItemLabel);
-    setActiveSection(menuItem!);
-  };
+    const user = getUser()
+    const handleActiveMenuItem = (menuItemLabel: string) => {
+        const menuItem = menuItems.find((item) => item.menuLabel === menuItemLabel);
+        setActiveSection(menuItem!);
+    };
 
-  return (
-    <Sidebar
-      rootStyles={{
-        height: "100vh",
-      }}
-    >
-      <Menu
-        className="pt-10"
-        menuItemStyles={{
-          button: {
-            [`&.ps-active`]: {
-              backgroundColor: "#F2F2F2", // grey-200
-              color: "#4CAF50", // primary-500
-            },
-          },
-          label: {
-            [`&.ps-active p`]: {
-              backgroundColor: "#F2F2F2", // grey-200
-              color: "#4CAF50", // primary-500
-            },
-          },
-        }}
-      >
-        <MenuItem component={<Link to="/settings" />}>
-          <img src={PocketmintLogo} className="w-full rounded cover" />
-        </MenuItem>
-        <MenuItem className="mt-3 mb-3">
-          <div className="flex flex-row items-center">
-            <img src={HeinrichProfilePicture} className="rounded cover" />
-            <p className="font-bold ml-3">Heinrich</p>
-          </div>
-        </MenuItem>
-        {menuItems.map((item) => (
-          <MenuItem
-            key={item.menuLabel}
-            active={item.menuLabel === activeSection.menuLabel}
-            icon={item.icon}
-            component={<Link to={item.link} />}
-            onClick={() => handleActiveMenuItem(item.menuLabel)}
-          >
-            <p className="font-medium text-grey-900">{item.menuLabel}</p>
-          </MenuItem>
-        ))}
-      </Menu>
-    </Sidebar>
-  );
+    return (
+        <Sidebar
+            rootStyles={{
+                height: "100vh",
+            }}
+        >
+            <Menu
+                className="pt-10"
+                menuItemStyles={{
+                    button: {
+                        [`&.ps-active`]: {
+                            backgroundColor: "#F2F2F2", // grey-200
+                            color: "#4CAF50", // primary-500
+                        },
+                    },
+                    label: {
+                        [`&.ps-active p`]: {
+                            backgroundColor: "#F2F2F2", // grey-200
+                            color: "#4CAF50", // primary-500
+                        },
+                    },
+                }}
+            >
+                <MenuItem component={<Link to="/settings" />}>
+                    <img src={PocketmintLogo} className="w-full rounded cover" />
+                </MenuItem>
+                <MenuItem className="mt-3 mb-3">
+                    <div className="flex flex-row items-center">
+                        <img src={user?.profilePicture} className="rounded w-[30px] h-[30px] object-cover" />
+                        <p className="font-bold ml-3">{user?.username}</p>
+                    </div>
+                </MenuItem>
+                {menuItems.map((item) => (
+                    <MenuItem
+                        key={item.menuLabel}
+                        active={item.menuLabel === activeSection.menuLabel}
+                        icon={item.icon}
+                        component={<Link to={item.link} />}
+                        onClick={() => handleActiveMenuItem(item.menuLabel)}
+                    >
+                        <p className="font-medium text-grey-900">{item.menuLabel}</p>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Sidebar>
+    );
 };
