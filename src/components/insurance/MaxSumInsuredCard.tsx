ac1091a09f1827b654205d55a4b691e2e17a81e0
ferusel <wheatgrass.mc@gmail.com>
2023-09-06 12:37:32 +0200

    Fix rendering of values

--- src/components/insurance/MaxSumInsuredCard.tsx
+++ src/components/insurance/MaxSumInsuredCard.tsx
@@ -7,14 +7,18 @@ interface MaxTotalPremium {
     "max_total_premiums": number
 }
 export interface InsuranceCoverageCardProps {
-    "life": number
-    "accident_and_health": number
-    "investments": number
-    "others": number
+    "life": number[]
+    "accident_and_health": number[]
+    "investments": number[]
+    "general": number[]
     className: string
 }
 
-export const MaxSumInsuredCard = ({ life, "accident_and_health": accidentAndHealth, investments, others, className }: InsuranceCoverageCardProps) => {
+export const MaxSumInsuredCard = ({ life, "accident_and_health": accidentAndHealth, investments, general, className }: InsuranceCoverageCardProps) => {
+    console.log(life)
+    console.log(accidentAndHealth)
+    console.log(investments)
+    console.log(general)
     return <Card className={className}>
         <CardHeader>
             <CardTitle className="text-lg">Maximum Sum Insured</CardTitle>
@@ -25,28 +29,28 @@ export const MaxSumInsuredCard = ({ life, "accident_and_health": accidentAndHeal
                     <img src="/src/assets/insurance/LifeLogo.svg" alt="Life Logo" className="w-[60px] h-[60px]" />
                     <div className="flex flex-col gap-2">
                         <span className="font-semibold text-md">Life</span>
-                        <p className="font-light">Term: ${life}</p>
+                        <p className="font-light">Term: ${life.length > 0 ? life : 0}</p>
                     </div>
                 </div>
                 <div className="flex flex-row gap-10">
                     <img src="/src/assets/insurance/AccidentAndHealthLogo.svg" alt="Accident and Health Logo" className="w-[60px] h-[60px]" />
                     <div className="flex flex-col gap-2">
                         <span className="font-semibold text-md">Accident and Health</span>
-                        <p className="font-light">Medical: ${accidentAndHealth}</p>
+                        <p className="font-light">Medical: ${accidentAndHealth.length > 0 ? accidentAndHealth : 0}</p>
                     </div>
                 </div>
                 <div className="flex flex-row gap-10">
                     <img src="/src/assets/insurance/InsuranceLogo.svg" alt="Accident and Health Logo" className="w-[60px] h-[60px]" />
                     <div className="flex flex-col gap-2">
                         <span className="font-semibold text-md">Investments</span>
-                        <p className="font-light">Total Payments: ${investments}</p>
+                        <p className="font-light">Total Payments: ${investments.length > 0 ? investments : 0}</p>
                     </div>
                 </div>
                 <div className="flex flex-row gap-10">
                     <img src="/src/assets/insurance/GeneralLogo.svg" alt="General Logo" className="w-[60px] h-[60px]" />
                     <div>
                         <span className="font-semibold text-md">General</span>
-                        <p className="font-light">General Policies: ${others}</p>
+                        <p className="font-light">General Policies: ${general.length > 0 ? general : 0}</p>
                     </div>
                 </div>
             </div>
